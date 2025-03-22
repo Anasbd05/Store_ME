72cb4a8a6cec2154c41c1484cfe9859dc851aaac
anas bd <anastrying05@gmail.com>
2025-03-22 17:35:18 +0000

    added card

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,8 +1,24 @@
+"use client"
+import {useCartStore} from '@/store/CartStore'
+import Image from 'next/image'
 import React from 'react'
 
 const CheckoutPage = () => {
+    const {items} = useCartStore()
     return (
-        <div>
+        <div className='w-2/4 h-screen'>
+            {items.map((item) =>
+                <div className='flex items-center w-full my-2 bg-gray-100 shadow-md py-2 px-1' key={item.id} >
+                    <Image className='w-16 rounded-md object-cover h-16' alt='' src={item.image || null} />
+                    <div className="flex w-full items-center px-4 justify-between">
+                        <div className="flex flex-col">
+                            <h1 className='text-lg font-semibold'>{item.name}</h1>
+                            <p className='font-bold'>${item.price} </p>
+                        </div>
+                        <p>{item.quantity}</p>
+                    </div>
+                </div>
+            )}
 
         </div>
     )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,15 @@
+"use client"
+import {useCartStore} from '@/store/CartStore'
+import {ShoppingCart} from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
+
+    const {items} = useCartStore()
+
+    const cartCount = items.reduce((acc,item) => acc + item.quantity,0)
+
     return (
         <nav className='flex px-2 sm:px-3 md:px-6 lg:px-8 py-4 shadow bg-white justify-between items-center'>
             <div>
@@ -15,7 +23,14 @@ const Navbar = () => {
                 <Link className='py-2 px-3 hover:bg-gray-200 rounded-md' href={"/checkout"}>Checkout</Link>
             </div>
             {/* shopping cart */}
-            <div></div>
+            <div>
+                <Link href={"/checkout"}>
+                    {cartCount > 0 && <span className=' bg-red-500 text-white w-2 h-2 px-2 rounded-full text-sm'>
+                        {cartCount}
+                    </span>}
+                    <ShoppingCart />
+                </Link>
+            </div>
         </nav>
     )
 }
